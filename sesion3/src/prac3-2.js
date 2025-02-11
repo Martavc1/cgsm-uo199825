b92f1d55f126cedcc1d8aad25ba8f46af3e8c5c1
Marta Vázquez <uo199825@uniovi.es>
2025-02-11 08:00:12 +0100

    prac 3

--- sesion3/src/prac3-2.js
+++ sesion3/src/prac3-2.js
@@ -1,12 +1,4 @@
 import * as THREE from 'three';
-import WEBGL from 'three/examples/jsm/capabilities/WebGL.js';
-
-if ( !WEBGL.isWebGL2Available() ) {
-    const nuevoDiv = document.createElement('div');
-    
-    nuevoDiv.textContent = WEBGL.getWebGL2ErrorMessage().textContent;
-    document.body.appendChild(nuevoDiv);
-}
 
 
 const scene = new THREE.Scene();
@@ -15,7 +7,7 @@ const renderer = new THREE.WebGLRenderer( {antialias: true} );
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const camera = new THREE.PerspectiveCamera ( 45, window.innerWidth / window.innerHeight, 1, 4000 );
+const camera = new THREE.PerspectiveCamera ( 65, window.innerWidth / window.innerHeight, 1, 4000 );
 camera.position.set( 0, 0, 300 );
 
 const video = document.getElementById( 'video' );
@@ -24,8 +16,6 @@ const image = document.createElement( 'canvas' );
 image.width = 480;  // Video width
 image.height = 204; // Video height
 const imageContext = image.getContext( '2d' );
-
- 
 imageContext.fillStyle = '#000000';
 imageContext.fillRect( 0, 0, image.width - 1, image.height - 1 );
 const texture = new THREE.Texture( image );
@@ -33,46 +23,37 @@ const texture = new THREE.Texture( image );
 
 const material = new THREE.MeshBasicMaterial( { map: texture } );
 const wall = new THREE.Mesh( new THREE.PlaneGeometry( image.width, image.height, 4, 4 ), material );
-wall.position.set( -50, 10, 50 );
-imageContext.drawImage( video, 0, 0 );
+//wall.position.set( 50, 10, 50 );
+wall.rotation.set( Math.PI / 5, Math.PI / 5, 0 );
 
+//const light = new THREE.PointLight( 0xffffff, 10, 1000,0 );
+//light.position.set( 20, 100, 500 );
+//scene.add( light );
 
-
-const light = new THREE.PointLight( 0xffffff, 100, 1000,0 );
-light.position.set( 20, 100, 500 );
-
-scene.add( light );
 scene.add(wall); 
-
-const light2 = new THREE.PointLight( 0xffffff, 20, 50,0 );
-const geometry = new THREE.BoxGeometry( 60, 60, 60 );
-light2.position.set( 10, 10, 40 );
 
 
 renderer.render( scene, camera );
 
-  function render() {
- 
+  function render(time) {
+    wall.rotation.y -= Math.PI * 0.5 / 180;  
     
     renderer.render(scene, camera);
 
-    requestAnimationFrame(render);
-  }
-  
-  requestAnimationFrame(render);
+	if ( video.readyState === video.HAVE_ENOUGH_DATA ) {
 
-  function animate() {
-    //requestAnimationFrame(animate)
-
-    if ( video.readyState === video.HAVE_ENOUGH_DATA ) {
- 
 		imageContext.drawImage( video, 0, 0 );
-		if ( texture ) 
-			texture.needsUpdate = true;
+		if ( texture ) texture.needsUpdate = true;
 	}
 
-    render()
+    requestAnimationFrame(render);
+  }
+  requestAnimationFrame(render);
 
-}
 
-animate()
+
+
+
+
+
+	requestAnimationFrame(render);
