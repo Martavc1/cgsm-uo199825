0e38ad7bb7daaa5eab154d9a88072598084955fe
Marta Vázquez <uo199825@uniovi.es>
2025-02-12 09:43:14 +0100

    prac 3

--- sesion3/src/prac3-3.js
+++ sesion3/src/prac3-3.js
@@ -1,97 +1,91 @@
-import * as THREE from 'three'
+import * as THREE from 'three';
+import WEBGL from 'three/examples/jsm/capabilities/WebGL.js'; 
 import { GUI } from 'dat.gui';
-
 import Stats from 'three/examples/jsm/libs/stats.module';
 
+if ( !WEBGL.isWebGL2Available() ) {
+    const nuevoDiv = document.createElement('div');
+    
+    nuevoDiv.textContent = WEBGL.getWebGL2ErrorMessage().textContent;
+    document.body.appendChild(nuevoDiv);
+}
 
-    const textureLoader = new THREE.TextureLoader( );  
-
-    // create a scene, that will hold all our elements such as objects, cameras and lights.
-    var scene = new THREE.Scene();
-
-    // create a camera, which defines where we're looking at.
-    var camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 1.1, 4000);
+const textureLoader = new THREE.TextureLoader( );  
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 1.1, 4000);
 
 
-	const stats = new Stats( );
+const stats = new Stats( );
 stats.domElement.style.position = 'absolute';
 stats.domElement.style.top = '0px';
 document.body.appendChild( stats.domElement );
     
-    const renderer = new THREE.WebGLRenderer( {antialias: true} );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
+const renderer = new THREE.WebGLRenderer( {antialias: true} );
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-  const mapUrlBrick = "../textures/brick.jpg";   // The file used as texture 
-  const mapBrick = textureLoader.load( mapUrlBrick );
-  const materialBrick = new THREE.MeshPhongMaterial( { map: mapBrick } );
+const mapUrlBrick = "../textures/brick.jpg";   // The file used as texture 
+const mapBrick = textureLoader.load( mapUrlBrick );
+const materialBrick = new THREE.MeshPhongMaterial( { map: mapBrick } );
 
-  const geometry = new THREE.BoxGeometry( 10, 10, 10 );
+const geometry = new THREE.BoxGeometry( 10, 10, 10 );
     
-  const cubo1 = new THREE.Mesh( geometry, materialBrick );
-  cubo1.rotation.y = 0.5;
-  cubo1.position.x = -12;
-  //cubo1.position.set( -50, 10, 50 );
-  scene.add(cubo1);
+const box1 = new THREE.Mesh( geometry, materialBrick );
+box1.rotation.y = 0.5;
+box1.position.x = -12;
+//box1.position.set( -50, 10, 50 );
+scene.add(box1);
 
-    var cubo2 = createMesh(new THREE.BoxGeometry(10, 10, 10), "brick.jpg", "brick-map.jpg");
-    cubo2.rotation.y = 0.5;
-    cubo2.position.x = 12;
-    scene.add(cubo2); 
+var box2 = createMesh(new THREE.BoxGeometry(10, 10, 10),  "brick-map.jpg");
+box2.rotation.y = 0.5;
+box2.position.x = 12;
+scene.add(box2); 
 
     // position and point the camera to the center of the scene
-    camera.position.x = 0;
-    camera.position.y = 15;
-    camera.position.z = 28;
-    camera.lookAt(new THREE.Vector3(0, 0, 0));
+camera.position.x = 0;
+camera.position.y = 15;
+camera.position.z = 28;
+camera.lookAt(new THREE.Vector3(0, 0, 0));
 
+const light = new THREE.PointLight( 0xffffff, 3, 1000,0 );
+light.position.set( 0, 100, 50 );
+scene.add(light);
 
-    const light = new THREE.PointLight( 0xffffff, 0.9, 1000,0 );
-    light.position.set( 20, 100, 50 );
-    scene.add(light);
+var controls = new function () {
+    this.bumpScale = 0.5;
 
+    this.updateBump = function (e) {              
+        box2.material.bumpScale = e; 
+    }
+};
 
-    var controls = new function () {
-        this.bumpScale = 0.5;
-
-        this.updateBump = function (e) {              
-            cubo2.material.bumpScale = e; 
-        }
-    };
-
-
-    var gui = new GUI( );
-    gui.add(controls, "bumpScale", -4, 4).onChange(controls.updateBump); 
+var gui = new GUI( );
+gui.add(controls, "bumpScale", -4, 4).onChange(controls.updateBump); 
  
-    render();
+render();
 
-    function createMesh(geom, imageFile, bump) { 
+function createMesh(geom, bump) { 
 
-        var texturem = "../textures/brick.jpg";
-        const texture = textureLoader.load( texturem );
+    var texturem = "../textures/brick.jpg";
+    const texture = textureLoader.load( texturem );
     
-        var mat = new THREE.MeshPhongMaterial();
-        mat.map = texture;
+    var mat = new THREE.MeshPhongMaterial();
+    mat.map = texture;
 
-        if (bump) { 
-
-                  var texturem = "../textures/" + bump;           
-        const bump2 = textureLoader.load( texturem );
-
-       
-            mat.bumpMap = bump2;
-            mat.bumpScale = 0.2; 
-        }
-
-        var mesh = new THREE.Mesh(geom, mat);
-        return mesh;
-    }
-
-    function render() { 
-        cubo1.rotation.y += 0.001;
-        cubo2.rotation.y -= 0.001;
-		stats.update( );
+    var texturem = "../textures/" + bump;           
+    const bump2 = textureLoader.load( texturem );
+    mat.bumpMap = bump2;
+    mat.bumpScale = 0.2; 
         
-        requestAnimationFrame(render);
-        renderer.render(scene, camera);
-    }
+    var mesh = new THREE.Mesh(geom, mat);
+    return mesh;
+}
+
+function render() { 
+    box1.rotation.y += 0.001;
+    box2.rotation.y -= 0.001;
+	stats.update( );
+        
+    requestAnimationFrame(render);
+    renderer.render(scene, camera);
+}
